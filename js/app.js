178d0a5458cbfa9301f080e11f1ff9098e7ac85d
Francisco Lazcano <marco.francisco.mendia@gmail.com>
2021-05-20 10:01:10 -0600

    Modificaciones de ruta b

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ if(navigator.serviceWorker)
     if( url.includes('localhost')) {
         swLocation = '/sw.js';
     }
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register(swLocation);
 }
 // Referencias de jQuery
 
